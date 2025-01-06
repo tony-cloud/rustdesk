87cb4680575fb76d818c49d97e74c412f89f7367
Tony <tonylu@tony-cloud.com>
2025-01-06 16:21:08 +0800

    fix: _updateUrl to updateUrl

--- flutter/lib/mobile/pages/connection_page.dart
+++ flutter/lib/mobile/pages/connection_page.dart
@@ -106,7 +106,7 @@ class _ConnectionPageState extends State<ConnectionPage> {
         ? const SizedBox(height: 0)
         : InkWell(
             onTap: () async {
-              final url = _updateUrl;
+              final url = updateUrl;
               // https://pub.dev/packages/url_launcher#configuration
               // https://developer.android.com/training/package-visibility/use-cases#open-urls-custom-tabs
               //
